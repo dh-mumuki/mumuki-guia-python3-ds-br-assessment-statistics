8635f58678a5d41307c1a5b03109e5ad44207cbf
vmatos@digitalhouse.com <vmatos@digitalhouse.com>
2021-05-27 04:37:42 +0000

    Mumuki Export on 2021-05-27 04:37:42 +0000

--- 00007_Condicional/test.py
+++ 00007_Condicional/test.py
@@ -3,7 +3,7 @@ class TestFixtures(unittest.TestCase):
     self.assertEqual(condicional(14), 3)
       
   def test_se_argumento_menos_11_retorna_menos_6(self):
-    self.assertEqual(condicional(-11), 28)
+    self.assertEqual(condicional(-11), -6)
   
   def test_se_argumento_22_retorna_25(self):
     self.assertEqual(condicional(22), 25)
